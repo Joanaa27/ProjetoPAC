4f33a3949c32d7d6e2fe9c3835574c1c66ea3813
Joanaa27 <joana271101@gmail.com>
2022-12-17 14:47:16 +0000

    Correção de algumas funções

--- Plots.py
+++ Plots.py
@@ -30,101 +30,117 @@ diabetes.insert(loc=2, column="GlycemiaValues", value=x)
 
 #boxplot de todas as variáveis em função da variavel "Outcome"
 #eliminar a coluna GlycemiaValues porque não queremos e não faz sentido aparecer na dataframe
-diabetesbp= diabetesdf.drop("GlycemiaValues", axis=1)
-diabetes_melted = pd.melt(diabetesbp, id_vars = "Outcome", var_name = "variables", value_name = "value")
-
-def boxplot_all():
+#dropvalues - é uma lista com as variaveis que o utilizador nao quer vizualizar, sendo que a variavel "GlycemiaValues é obrigatória de descartar
+def boxplot_all(dataframe,drop_values, has_outcome=False):
+   
+    diabetesbp= dataframe.drop(drop_values, axis=1)
+    diabetes_melted = pd.melt(diabetesbp, id_vars = "Outcome", var_name = "variables", value_name = "value")
+    
     plt.figure(figsize = (15, 15))
-    sns.boxplot(data = diabetes_melted, x="variables", y="value", hue="Outcome")
-    plt.title("Boxplot of all numeric variables in order to Outcome")
+
+    sns.boxplot(data = diabetes_melted, x="variables", y="value", hue="Outcome") if has_outcome else sns.boxplot(data = diabetes_melted, x="variables", y="value")
+    plt.title("Boxplot of numeric variables by Outcome") if has_outcome else plt.title("Boxplot of numeric variables")
     plt.show()
 
-#swarm plot de todas as variavem em função da variável "Outcome"
-def swarvar():
+#boxplot_all(diabetesdf)
+
+
+#stripplot de todas as variavem em função da variável "Outcome"
+#eliminar a coluna GlycemiaValues porque não queremos e não faz sentido aparecer na dataframe0
+def stripvar(dataframe, drop_values, has_outcome=False):
+    diabetesbp= dataframe.drop(drop_values, axis=1)
+    diabetes_melted = pd.melt(diabetesbp, id_vars = "Outcome", var_name = "variables", value_name = "value")
+    
     plt.figure(figsize = (15, 15))
-    sns.swarmplot(x = "variables", y = "value", hue = "Outcome", data = diabetes_melted)
-    plt.title("Swarmplot of all variavles by Outcome")
+    sns.stripplot(x = "variables", y = "value", hue = "Outcome", data = diabetes_melted) if has_outcome else sns.stripplot(x = "variables", y = "value", data = diabetes_melted)
+    plt.title("Stripplot of numeric variables by Outcome")  if has_outcome else plt.title("Stripplot of numeric variables")
     plt.show()
+
+#stripvar(diabetesdf,["GlycemiaValues","Insulin"],True)
 
 #Gráfico de barras com a contagem de outcomes ou de GlycemiaValues
-def outc_values(vcategorical):
+def outc_values(dataframe, vcategorical):
     order = []
     if vcategorical=="Outcome":
         order=[0,1]
     else:
         order=['Hypoglycemia','Normal','Pre-diabetes']
     plt.figure()
-    sns.countplot(x = diabetesdf[vcategorical], data = diabetesdf, hue_order=order)
+    sns.countplot(x = dataframe[vcategorical], data = dataframe, hue_order=order)
     plt.title(f"Distribution of {vcategorical}")
     plt.show()
 
 #outc_values("GlycemiaValues")
 
 #grafico circular do outcome 0 e 1  e da variavel criada "GlycemiaValues"
-def circular():
-    labels= 'Not Diabetic','Diabetic'
+def circular(dataframe, vcategorical):
+    labels=[]
+    if vcategorical=="Outcome":
+        labels= ['Not Diabetic','Diabetic'] 
+    else:
+        labels=['Hypoglycemia','Normal','Pre-diabetes']
+
     plt.figure(figsize=(10,7))
-    plt.pie(diabetesdf['Outcome'].value_counts(),labels=labels,autopct='%0.02f%%')
+    plt.pie(dataframe[vcategorical].value_counts(),labels=labels,autopct='%0.02f%%')
     plt.legend()
     plt.show()
-######Nota! Gráfico redundante porque já temos um barplot a dizer isto --> escolher qual usar
 
-#separam o outcome 0 de 1
-df_d0 = diabetes[diabetes['Outcome'] == 0]
-df_d1 = diabetes[diabetes['Outcome'] == 1]
-
-#and random sample the same amount of instances from the higher represented '0' class to match the available '1' class instances
-df_d0_samp = df_d0.sample(268,replace = False)
-df_bal = pd.concat([df_d1, df_d0_samp])
+#print((diabetesdf["GlycemiaValues"]=="Normal").value_counts()) #560
 
 #histograma com função de densidade das variáveis em função do outcome
-def histvout(): 
-    cores = {0: 'blue', 1: 'green'}
-    cores2 = {'Hypoglycemia': 'blue', 'Normal': 'green', 'Pre-diabetes': 'yellow'}
-    variaveisnum= diabetes.columns[0:8]
+def histvout(dataframe, vcategorical): 
+
+    if vcategorical=="Outcome":
+        #separam o outcome 0 de 1
+        df_d0 = dataframe[dataframe['Outcome'] == 0]
+        df_d1 = dataframe[dataframe['Outcome'] == 1]
+        #executamos uma amostra aleatoria com o mesmo numero de observações da categoria mais representada, 
+        #neste caso 0 para dar "match" com as observações da categoria 1
+        df_d0_samp = df_d0.sample(268,replace = False)
+        df_bal = pd.concat([df_d1, df_d0_samp])
+        cores = {0: 'blue', 1: 'green'}
+    else:
+        df_d0 = dataframe[dataframe['GlycemiaValues'] == "Hypoglycemia"]
+        df_d1 = dataframe[dataframe['GlycemiaValues'] == "Normal"]
+        df_d2 = dataframe[dataframe['GlycemiaValues'] == "Pre-diabetes"]
+        df_d1_samp = df_d1.sample(560,replace = False)
+        df_bal = pd.concat([df_d0,df_d2, df_d1_samp])
+        cores = {'Hypoglycemia': 'yellow', 'Normal': 'green', 'Pre-diabetes': 'blue'}
+
+
+    variaveis= diabetes.columns #usamos diabetes, porque nao queremos a variavel que criamos
     counter = 0
-    for i in variaveisnum:
+    for val in variaveis:
         counter += 1
-        print(counter, ':', i)
-        sns.displot(data = df_bal, kde=True, x = diabetes[str(i)], hue='Outcome', palette=cores)
-        plt.title(f'"{i}" em função do Outcome')
+        print(counter, ':', val)
+        sns.displot(data = df_bal, kde=True, x = diabetes[str(val)], hue=vcategorical, palette=cores)
+        plt.title(f'"{val}" by {vcategorical}')
     plt.plot()
     plt.show()
 
-#Histogramas + boxplots (lado a lado) para uma variável numerica em função do outcome
-def histpx():
-    fig = px.histogram(diabetesdf, x = 'Glucose',
-                    color = 'Outcome',
-                    marginal = 'box',
-                    barmode= 'overlay',
-                    histnorm = 'density'
-                    )
-
-    fig.update_layout(
-        title_font_color="black",
-        legend_title_font_color="green",
-        title={
-            'text': "Glucose Histogram per Outcome",
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'},
-    )
-    fig.show()
-
-#Problema: abre uma página no navegador e temos de ver se é dessa maneira que queremos visualizar o gráfico
+#histvout(diabetesdf,"GlycemiaValues")
 
 # Pairplot
-def pairplt():
-    cores = {0: 'blue', 1: 'green'}
+def pairplt(dataframe,vcategorical=None):
+    if vcategorical=="Outcome":
+        cores = {0: 'blue', 1: 'green'}
+    elif vcategorical=="GlycemiaValues":
+        cores = {'Hypoglycemia': 'yellow', 'Normal': 'green', 'Pre-diabetes': 'blue'}
+    else:
+        cores = None
+
     plt.figure()
     sns.set(font_scale=0.7)
-    sns.pairplot(diabetesdf, hue = "Outcome", diag_kind = "kde", palette = cores, plot_kws = {"s": 8})
-    plt.title('Pairplot')
+    sns.pairplot(dataframe, hue = vcategorical, diag_kind = "kde", palette = cores, plot_kws = {"s": 8})
+    plt.title(f"Pairplot of all numeric variables by {vcategorical}")
     plt.show()
 
+#pairplt(diabetes#df,"GlycemiaValues")
+#pairplt(diabetesdf)
+
 #Matriz de correlações
-def matrcorr():
-    corr = diabetesdf.corr().round(2)
+def matrcorr(dataframe):
+    corr = dataframe.corr().round(2)
     plt.figure(figsize=(14, 10))
     sns.set(font_scale=1.15)
     mask = np.zeros_like(corr)
@@ -132,6 +148,8 @@ def matrcorr():
     sns.heatmap(corr, annot = True, cmap = 'BuPu', mask = mask, cbar = True)
     plt.title('Correlation Matrix')
     plt.show()
+
+#matrcorr(diabetesdf)
 
 #função que executa gráficos de dispersão entre as variaveis NUMERICAS escolhidas pelo utilizador
 def varscatter(variavel_1,variavel_2):
@@ -153,3 +171,25 @@ def varscatter2(variavel1, variavel2, varcategorical):
 
 #Nestas funções, as variáveis deviam ser pedidas pelo utlizador, ou seja input, 
 #e depois eram feitas verificações caso o nome da variável não fosse correto
+
+
+#Histogramas + boxplots (lado a lado) para uma variável numerica em função do outcome
+def histpx():
+    fig = px.histogram(diabetesdf, x = 'Glucose',
+                    color = 'Outcome',
+                    marginal = 'box',
+                    barmode= 'overlay',
+                    histnorm = 'density'
+                    )
+
+    fig.update_layout(
+        title_font_color="black",
+        legend_title_font_color="green",
+        title={
+            'text': "Glucose Histogram per Outcome",
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+    )
+    fig.show()
+#Problema: abre uma página no navegador e temos de ver se é dessa maneira que queremos visualizar o gráfico
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import plotly_express as px
 from Plots import *
 from Funçoes import *
 
-def printMenu():
+def menu():
     print("        [Menu] Escolhe uma das seguintes opções:")
     print("|                  0 - Visualização da DataFrame Diabetes                       |")
     print("|                  1 - Variables                      |")
@@ -19,26 +19,28 @@ def printMenu():
     print("|                  4 - Sample                         |")
     print("|                  5 - Report                         |")
     print("|                  6 - Sair                         |")
+    opcoes_menu()
 
 #opção 0 - visualização da data frame e de algumas informações relativas à mesma
-def menu():
+def opcoes_menu():
     opcao= int(input("Opção:"))
     while opcao < 0 and opcao >= 6:
         print("Tem de escolher uma opção entre 0 e 6")
-        printMenu()
-        opcao = int(input("Opção:"))
+        menu()
     
     if opcao == 0:
         print(f"Base de Dados Diabetes:\n {diabetesdf}")
         print(f"Informações gerais sobre a base de dados:\n {diabetesdf.info()}")
         opcaoa= int(input("Pretende ainda visualizar:\n 0- tabela com as primeiras 20 observações \n 1- tabela com cálculos estatísticos das variáveis \n 2-Voltar ao Menu Principal \n Opção:"))
         if opcaoa==0:
-            print(f"Tabela com as primeiras 20 observações da base de dados:\n {tabelas(diabetesdf,1)}")
+            print(f"Tabela com as primeiras 20 observações da base de dados\n {tabelas(diabetesdf,1)}")
+            terminar()
         elif opcaoa==1:
-            print(f"Tabela com as primeiras 20 observações da base de dados:\n {tabelas(diabetesdf,2)}")
+            print(f"Tabela com as primeiras 20 observações da base de dados\n {tabelas(diabetesdf,2)}")
+            terminar()
         else:
-            #stop()
-            printMenu()
+            exit()
+        menu()
     
     elif opcao == 1:
         variavel= int(input("Escolha uma das seguintes opções:\n 0-Análise Geral das Variáveis Numéricas \n 1- "))
@@ -46,18 +48,35 @@ def menu():
             print(diabetesdf.describe())
         elif variavel==1:
             print("xau")
+    
     #elif opcao == 2:
     #    função3
+    
     #elif opcao == 3:
     #    função4
+    
     elif opcao == 5:
         #geração de um relatório automático sobre a analise da base de dados
         relatorio = ProfileReport(diabetesdf, title="Data Analysis of Diabetes Report")
         diabetesdf.profile_report()
         relatorio.to_file("diabetes_report.html")
+    
+    elif opcao == 6:
+        exit()
     #else:
 
-printMenu()
 menu()
 
-#fazer loop para estar sempre a repetir até a pessoa escolher a opção sair
+#função para todos os fins de opção perguntar se quer continuar ou terminar a analise
+def terminar():
+    escolha=input("Deseja continuar a análise estatística? Escreva 'Sim' para continuar ou 'Não' para terminar \n")
+    escolha = escolha.lower()
+    while escolha != "sim" and escolha != "s" and escolha != "não" and escolha != "n" and escolha != "nao":
+        print("Resposta inválida. Escreva 'Sim' para continuar a análise e 'Não' para terminar. \n")
+        escolha=input("Deseja continuar a análise estatística? Escreva 'Sim' para continuar ou 'Não' para terminar \n")
+        escolha = escolha.lower()
+
+    if escolha == "sim" or escolha == "s":
+        menu()
+    elif escolha == "Não":
+        exit()
